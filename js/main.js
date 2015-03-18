f2153995ee325d0dec0be395fea3e53a6c93a2f1
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2015-03-18 13:42:18 -0400

    built lightbox

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@ var $navBtn = $('.nav-btn');
 var $menu = $('.menu');
 var $main = $('.main');
 
+var $thumbs = $('.thumbnails');
+var $lbImg = $('.lightbox-img');
+var $lb = $('.lightbox');
+var $btnClose = $('.btn-close');
+
 console.log('loaded js');
 
 $enter.on('click', function (e) {
@@ -25,3 +30,14 @@ if(sessionStorage.getItem("active")){
 	$banner.addClass('entered');
 	$banner.hide();
 }
+
+$thumbs.on('click', 'a', function (e) {
+	e.preventDefault();
+	var big = $(this).attr('href');
+	$lbImg.attr('src', big);
+	$lb.attr('data-state', 'visible');
+});
+
+$btnClose.on('click', function () {
+	$lb.attr('data-state', 'hidden');
+});
