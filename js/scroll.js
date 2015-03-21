ac90b717e5a986bc5c79a3534556fabb8d9a029e
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2015-03-21 12:09:41 -0400

    added webkit stuff in js

--- js/scroll.js
+++ js/scroll.js
@@ -4,6 +4,8 @@ var $back = $('.back');
 
 $('.main').scroll(function(){
 	console.log($(this).scrollTop());
+	$mid.css('-webkit-filter', 'blur(' + $(this).scrollTop() / 100 + 'px)');
+	$back.css('-webkit-filter', 'blur(' + $(this).scrollTop() / 150 + 'px)');
 	$mid.css('filter', 'blur(' + $(this).scrollTop() / 100 + 'px)');
 	$back.css('filter', 'blur(' + $(this).scrollTop() / 150 + 'px)');
 });
