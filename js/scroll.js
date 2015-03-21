29e19842b4e4ec567ab14a19d44cba1568278ac1
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2015-03-21 09:58:09 -0400

    fixed scrolling issue when nav is open/large

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,9 @@
 var $scroll = $('.main').scrollTop();
 var $mid = $('.middle');
+var $back = $('.back');
 
 $('.main').scroll(function(){
 	console.log($(this).scrollTop());
 	$mid.css('filter', 'blur(' + $(this).scrollTop() / 40 + 'px)');
+	$back.css('filter', 'blur(' + $(this).scrollTop() / 150 + 'px)');
 });
