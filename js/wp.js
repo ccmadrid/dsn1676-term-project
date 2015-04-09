6fd144ea601b694baad08e19322bd0839daa67c9
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2015-04-09 15:56:31 -0400

    added random number generator for animation delay

--- js/wp.js
+++ js/wp.js
@@ -1,7 +1,13 @@
 var $bcSection = $('.bc-section');
+var $items = $('.bc g');
 
 $bcSection.waypoint(function () {
 	console.log('check');
 	$bcSection.toggleClass('js-go');
 
 }, { offset: '60%', context:'.main' });
+
+$items.each(function () {
+	var numRand = Math.floor(Math.random() * 2000);
+	$(this).css('animation-delay', numRand + 'ms');
+})
