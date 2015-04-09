735937d4e4ebae7b4396f3ad8205cac08d2636da
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2015-04-09 16:35:00 -0400

    added second ring

--- js/wp.js
+++ js/wp.js
@@ -7,6 +7,8 @@ $bcSection.waypoint(function () {
 }, { offset: '60%', context:'.main' });
 
 $items.each(function () {
-	var numRand = Math.floor(Math.random() * 2000);
+	var numRand = Math.floor(Math.random() * 1000);
+	var numRand2 = Math.floor(Math.random() * 200) +100;
 	$(this).css('animation-delay', numRand + 'ms');
+	$(this).css('animation-duration', numRand2 + 'ms');
 })
